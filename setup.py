b9e4b768eabe2f2a76188ed4e9728c00f5a2caef
jianjin <jxj362@case.edu>
2020-03-15 17:12:14 -0400

    update download url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Jian Jin',                   # Type in your name
   author_email = 'jjtt0926@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/JJ0131/Cbastats',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/JJ0131/Cbastats/archive/0.01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/JJ0131/Cbastats/archive/v0.0.2.tar.gz',    # I explain this later on
   keywords = ['CBA', 'Baseketball'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas','tabulate'
